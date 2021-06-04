80ad4ceace97ae2da69538c5fd1b7c22c3eb6b9e
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2021-06-04 08:01:46 +0200

    Updated country_data exercise

--- part2/country_data/src/App.js
+++ part2/country_data/src/App.js
@@ -2,66 +2,42 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Country = ( {country, state} ) => {
+  console.log("Call to Country component")
   const [show, setShow] = useState(state)
   const [weather, setWeather] = useState({})
 
   const api_key = process.env.REACT_APP_WEATHER_API_KEY
   console.log(api_key)
 
-  useEffect( () => {
-    axios
-      .get(`https://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}}`)
-      .then( response => { setWeather(response.data) } )
-  })
-
-  /*The code below was used for testing
-  useEffect( () => {
-    if(api_key != "1010") {
+  const updateWeatherData = () => {
+    console.log("Call to updateWeatherData")
+    if(api_key !== "1010") {
       console.log("Wrong api key")
       return
     }
-    setWeather( {
-      "request": {
-          "type": "City",
-          "query": `${country.capital}, ${country.name}`,
-          "language": "en",
-          "unit": "m"
-      },
-      "location": {
-          "name": `${country.capital}`,
-          "country": `${country.name}`,
-          "region": "",
-          "lat": "",
-          "lon": "",
-          "timezone_id": "",
-          "localtime": "",
-          "localtime_epoch": 0,
-          "utc_offset": ""
-      },
-      "current": {
-          "observation_time": "",
-          "temperature": 14,
-          "weather_code": 113,
-          "weather_icons": [
-              "https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png"
-          ],
-          "weather_descriptions": [
-              "Sunny"
-          ],
-          "wind_speed": 85,
-          "wind_degree": 349,
-          "wind_dir": "N",
-          "pressure": 1010,
-          "precip": 0,
-          "humidity": 90,
-          "cloudcover": 0,
-          "feelslike": 13,
-          "uv_index": 4,
-          "visibility": 16
-      }
-    } )
-    console.log(weather)
-  }) */
+    const cancelToken = axios.CancelToken
+    const source = cancelToken.source()
+    const url = `https://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}}`
+    // const url = `http://localhost:3001/weatherData/${country.capital}` - used for testing
+    axios
+      .get( url, {
+        cancelToken: source.token
+      })
+      .then( response => { 
+        console.log("weather data response", response)
+        setWeather( response.data )
+      } )
+      .catch( error => {
+        console.log("failed to obtain weatherData from api")
+        setWeather( {} )
+      })
+
+    return () => {
+      source.cancel(`api request to ${url} was cancelled as the component unmounted`);
+      };
+  }
+
+  useEffect( updateWeatherData, [] )
 
   if(!show) {
     return (
@@ -70,33 +46,47 @@ const Country = ( {country, state} ) => {
         <button onClick={ () => {setShow(true)} }>show</button>
       </div>
     )
-  } else if (Object.keys(weather).length === 0) {
-    // check if weather object has been initialized, since useEffect is only called after render
-    // we would be calling undefined properties of the weather object the first time the component
-    // is rendered, or any time the api request fails at first
-    console.log("weather", weather)
-    return ( null )
   } else {
+    let displayWeather = true
+    if (Object.keys(weather).length === 0) { displayWeather = false }
     console.log("weather", weather)
-    return (
-      <div>
-        <h2>{country.name}</h2>
-        <button onClick={ () => {setShow(false)} }>hide</button>
-        <p>
-          capital: {country.capital}<br />
-          population: {country.population}
-        </p>
-        <h3>languages</h3>
-        <ul>
-          {country.languages.map( (language, index) => <li key={index}>{language.name}</li>)}
-        </ul>
-        <img src={country.flag} width="150" />
-        <h3>Weather in {country.capital}</h3>
-        <strong>temperature: </strong>{weather.current.temperature} Celcius <br />
-        <img src={weather.current.weather_icons[0]} /> <br />
-        <strong>wind: </strong> {Math.round(weather.current.wind_speed*0.621371)} mph {weather.current.wind_dir}
-      </div>
-    )
+    if(displayWeather) {
+      return (
+        <div>
+          <h2>{country.name}</h2>
+          <button onClick={ () => { setShow(false)} }>hide</button>
+          <p>
+            capital: {country.capital}<br />
+            population: {country.population}
+          </p>
+          <h3>languages</h3>
+          <ul>
+            {country.languages.map( (language, index) => <li key={index}>{language.name}</li>)}
+          </ul>
+          <img src={country.flag} alt={`Flag of ${country.name}`} width="150" />
+          <h3>Weather in {country.capital}</h3>
+          <strong>temperature: </strong>{weather.current.temperature} Celcius <br />
+          <img src={weather.current.weather_icons[0]} alt={`Weather icon for ${weather.current.weather_descriptions[0]}`} /> <br />
+          <strong>wind: </strong> {Math.round(weather.current.wind_speed*0.621371)} mph {weather.current.wind_dir}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h2>{country.name}</h2>
+          <button onClick={ () => {setShow(false)} }>hide</button>
+          <p>
+            capital: {country.capital}<br />
+            population: {country.population}
+          </p>
+          <h3>languages</h3>
+          <ul>
+            {country.languages.map( (language, index) => <li key={index}>{language.name}</li>)}
+          </ul>
+          <img src={country.flag} width="150" />
+        </div>
+      )
+    }
   }
 }
 
@@ -107,7 +97,7 @@ const Results = ( {results} ) => {
   else if (results.length  > 10)  { return (<p>Too many matches, specify another filter</p>) }
   else { return (
       <>
-        {results.map( (result, index) => <Country key={index} country={result} state={false} />)}
+        {results.map( (result, index) => <Country key={result.name} country={result} state={false} />)}
       </>
     )
   }
@@ -117,9 +107,10 @@ const Results = ( {results} ) => {
 
 const App = () => {
 
+  const [data, setData] = useState([])
+
   const [search, setSearch] = useState('')
   const [results, setResults] = useState([])
-  const [data, setData] = useState([])
 
   const getData = () => {
     axios
@@ -129,12 +120,19 @@ const App = () => {
         setData(response.data)
         console.log(data)
       } )
+      .catch( error => {
+        console.log("failed to get initial data from api")
+      })
   }
 
   const updateResults = () => {
     console.log("updating results...")
-    setResults( data.filter( country => country.name.toLowerCase().includes( search.toLowerCase() ) ) )
-    console.log(results)
+    if(search.length === 0) {
+      setResults( [] )
+    } else {
+      setResults( data.filter( country => country.name.toLowerCase().includes( search.toLowerCase() ) ) )
+      console.log("updated countryData", results)
+    }
   }
 
   useEffect(getData, [])
@@ -143,7 +141,7 @@ const App = () => {
   return (
     <div>
       find countries <input value={search} onChange={ (event) => {
-          console.log(event.target.value)
+          console.log("search", event.target.value.length)
           setSearch(event.target.value)
         }
       }/>
