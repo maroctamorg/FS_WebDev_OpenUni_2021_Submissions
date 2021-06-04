fb7f80f83c28f6c4b9d8ebea3a0a727f3a7272ee
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2021-06-04 09:41:40 +0200

    Implemented exercises 2.15-2.16-2.17-2.18 in phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import phService from './services/phonebook'
 
 const Filter = (props) => <div>filter shown with <input value={props.search} onChange={props.handler}/></div>
 
@@ -17,11 +17,18 @@ const PersonForm = (props) => (
   </form>
 )
 
-const Person = ( {name, number} ) => <>{`${name} ${number}`}<br /></>
+const Person = ( {person, deleteEntry} ) => {
+  
+  return (
+    <>
+      {`${person.name} ${person.number}`} <button onClick={() => deleteEntry(person.id)}>delete</button> <br />
+    </>
+  )
+}
 
 const Persons = (props) => (
   <div>
-    {props.persons.map( person => <Person key={person.name} name={person.name} number={person.number}/>)}
+    {props.persons.map( person => <Person key={person.id} person={person} deleteEntry={props.deleteEntry}/>)}
   </div>
 )
 
@@ -33,49 +40,89 @@ const App = () => {
   const [ search, setNewSearch ] = useState('')
 
   useEffect( () => {
-    axios
-      .get( "http://localhost:3001/persons" )
-      .then( response => { setPersons(response.data) })
+    phService
+      .getAll()
+      .then( phonebook => setPersons( phonebook ) )
+      .catch( error => { alert("Unable to fetch phonebook from the server. Please refresh the page and try again!") } ) 
   }, [])
-
-  const updateSearch = (event) => {
-    console.log(event.target.value)
-    setNewSearch(event.target.value)
-  }
 
   const addPerson = (event) => {
     event.preventDefault() // prevents default form behaviour which is to submit a post request
-    console.log('button clicked', event.target)
 
-    if ( persons.map( person => person.name ).includes(newName) ) {
-      alert( `${newName} is already added to the phonebook` )
-
-      return
-    }
-
-    const person = {
+    const newPerson = {
       name: newName,
       number: newNumber
     }
 
-    setPersons(persons.concat(person))
-    setNewName('')
-    setNewNumber('')
+    if ( persons.map( person => person.name ).includes(newPerson.name) ) {
+      if ( persons.map( person => person.number).includes(newPerson.number) ){
+        alert( `The entry ${newName} ${newNumber} is already added to the phonebook` )
+      } else {
+        const confirmed = window.confirm(`Update phone number for entry ${newPerson.name} to ${newPerson.number}?`)
+        if (confirmed) {
+          phService
+          .update( persons.find( person => person.name === newPerson.name).id, newPerson )
+          .then( returnedPerson => {
+            setPersons(persons.map( person => person.name === newPerson.name ? returnedPerson : person ))
+            setNewName('')
+            setNewNumber('')
+          } )
+          .catch( error => {
+            alert(`Error updating the number for the entry: ${newPerson.name} to the phonebook. Please try again!`)
+          })
+        }
+      }
+      
+    } else {
+
+      phService
+        .create(newPerson)
+        .then( returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        } )
+        .catch( error => {
+          alert(`Error submitting the new entry: ${newPerson.name} ${newPerson.number} to the phonebook. Please try again!`)
+        })
+    }
   }
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
+  const deleteEntry = (id) => {
+    const personToDel = persons.find( person => person.id === id)
+    const confirmed = window.confirm(`Delete entry ${personToDel.name} ${personToDel.number}?`)
+    
+    if(!confirmed) { 
+      console.log(`the deletion of entry ${personToDel.name} ${personToDel.number} was cancelled`)
+    } else {
+      phService
+      .deleteEntry(id)
+      .then( response => {
+          console.log(response)
+          setPersons( persons.filter( person => person.id !== personToDel.id ) )
+        }
+      ).catch( error => {
+          alert(`Unable to delete entry ${personToDel.name} ${personToDel.number} from the server. Please try again!`)
+      })
+    }
   }
 
   const personsToShow = ( search ) => search === ''
     ? persons
     : persons.filter( person => person.name.toLowerCase().includes( search.toLowerCase() ))
+
+
+  const updateSearch = (event) => {
+    setNewSearch(event.target.value)
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
   return (
     <div>
@@ -84,7 +131,7 @@ const App = () => {
       <h3>add a new entry</h3>
       <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h3>Numbers</h3>
-      <Persons persons={personsToShow(search)} />
+      <Persons persons={personsToShow(search)} deleteEntry={deleteEntry}/>
     </div>
   )
 }
